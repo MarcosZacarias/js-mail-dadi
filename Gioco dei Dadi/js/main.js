2f02b291ea5286a90ee0a01cc72750699fde92a8
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-07-24 17:35:24 +0200

    add: name player and button

--- Gioco dei Dadi/js/main.js
+++ Gioco dei Dadi/js/main.js
@@ -1,24 +1,34 @@
 // Add Player 1
-const playerUtent = prompt("Inserisci il tuo nome da giocatore");
-console.log("Giocatore 1: " + playerUtent);
+const playerUtentInput = document.getElementById("player-1");
+console.log("Giocatore 1: " + playerUtentInput.value);
+
+let playerUtent;
 
 // Add Player 2
 const playerPC = "Computer";
 console.log("Giocatore 2: " + playerPC);
 
-// Generate number random Player 1
-const numberUtent = Math.floor(Math.random() * 6) + 1;
-console.log(playerUtent + " ha fatto: " + numberUtent);
+// Button send
+const btnSend = document.getElementById("btn-send");
 
-// Generate number random Player 2
-const numberPC = Math.floor(Math.random() * 6) + 1;
-console.log(playerPC + " ha fatto: " + numberPC);
+btnSend.addEventListener("click", function () {
+  // Player 1
+  playerUtent = playerUtentInput.value;
 
-// Select winner
-if (numberUtent > numberPC) {
-  console.log("Vincitore " + playerUtent);
-} else if (numberUtent == numberPC) {
-  console.log("Pareggio");
-} else {
-  console.log("Vincitore " + playerPC);
-}
+  // Generate number random Player 1
+  const numberUtent = Math.floor(Math.random() * 6) + 1;
+  console.log(playerUtent + " ha fatto: " + numberUtent);
+
+  // Generate number random Player 2
+  const numberPC = Math.floor(Math.random() * 6) + 1;
+  console.log(playerPC + " ha fatto: " + numberPC);
+
+  // Select winner
+  if (numberUtent > numberPC) {
+    console.log("Vincitore " + playerUtent);
+  } else if (numberUtent == numberPC) {
+    console.log("Pareggio");
+  } else {
+    console.log("Vincitore " + playerPC);
+  }
+});
